191e90c7c3a6dca9353bceb28f6246b59e99c40e
Sergio Martin Rubio <smartinrubio@hotels.com>
2018-12-28 18:24:01 +0000

    add dynamodb bean to prod config

--- src/main/java/org/smartinrubio/springbootdynamodb/config/DynamoDBConfigProd.java
+++ src/main/java/org/smartinrubio/springbootdynamodb/config/DynamoDBConfigProd.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -48,6 +49,11 @@ public class DynamoDBConfigProd {
                 .withCredentials(amazonAWSCredentialsProvider())
                 .withRegion(Regions.US_WEST_2)
                 .build();
+    }
+
+    @Bean
+    public DynamoDB dynamoDB() {
+        return new DynamoDB(amazonDynamoDB());
     }
 
     public AWSCredentialsProvider amazonAWSCredentialsProvider() {
